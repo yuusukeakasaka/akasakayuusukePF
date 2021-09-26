3d01d40e726d04201e5f79a31e0bb06a5dbfe04f
yuusukeakasaka <ayazato2206a@gmail.com>
2021-09-26 10:00:55 +0000

    [Fix]routes.rb

---
+++ app/models/public/user.rb
@@ -0,0 +1,6 @@
+class Public::User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
